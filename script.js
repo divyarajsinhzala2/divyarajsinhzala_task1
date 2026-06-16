c856e66fc1d931a3a5a212fe9d700a057d2d8380
divyarajsinhzala2 <zdivyaraj169@gmail.com>
2026-06-16 16:46:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (btn.textContent === "C") {
+      display.value = "";
+    } else if (btn.textContent === "=") {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = "Error";
+      }
+    } else {
+      display.value += btn.textContent;
+    }
+  });
+});
